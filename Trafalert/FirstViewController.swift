4408c916f8e4f2c6475b00ab9e22689e058a936a
Tomi Lahtinen <tomijlahtinen@gmail.com>
2016-01-28 21:19:10 +0200

    Initial Commit

---
+++ Trafalert/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Trafalert
+//
+//  Created by Tomi Lahtinen on 28/01/16.
+//  Copyright © 2016 Tomi Lahtinen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
