9b162d0833d08d887fc97c41cf8c5893adad2336
Tomi Lahtinen <tomijlahtinen@gmail.com>
2016-01-31 01:11:16 +0200

    Lots of domain stuff.. attempt to use WSDL.. no success so far

--- Server/src/main/java/com/studiowannabe/trafalert/Application.java
+++ Server/src/main/java/com/studiowannabe/trafalert/Application.java
@@ -2,14 +2,18 @@ package com.studiowannabe.trafalert;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Created by Tomi on 30/01/16.
  */
 
 @EnableAutoConfiguration
+@ComponentScan
+@EnableScheduling
 public class Application {
-
+    
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
