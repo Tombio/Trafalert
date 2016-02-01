8e04b5b03f9eb66dac3fefef2fb7c20cee009692
Tomi Lahtinen <tomijlahtinen@gmail.com>
2016-02-02 00:18:55 +0200

    Added WeatherInfo domain class

---
+++ iOS Client/Trafalert/WeatherInfo.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherInfo.swift
+//  Trafalert
+//
+//  Created by Tomi Lahtinen on 01/02/16.
+//  Copyright © 2016 Tomi Lahtinen. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherInfo {
+
+}
