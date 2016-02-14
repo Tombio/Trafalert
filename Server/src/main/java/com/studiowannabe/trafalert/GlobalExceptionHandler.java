4a96c9d4938821f29626507ee365ceb82b437a00
Tombio <tomijlahtinen@gmail.com>
2016-02-14 20:16:23 +0200

    Error handling and logging

--- Server/src/main/java/com/studiowannabe/trafalert/GlobalExceptionHandler.java
+++ Server/src/main/java/com/studiowannabe/trafalert/GlobalExceptionHandler.java
@@ -1,25 +1,29 @@
 package com.studiowannabe.trafalert;
 
+import lombok.extern.apachecommons.CommonsLog;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * Created by Tomi on 03/12/15.
  */
+@CommonsLog
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)  // 500
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(UnauthorizedAccessException.class)
-    public void handleUnauthrozedAccess() {
-        System.out.println("Unauthorized access");
+    public void handleUnauthorizedAccess(final HttpServletRequest req, final UnauthorizedAccessException exception) {
+        log.error("Unauthorized access from " + req.getRemoteAddr(), exception);
     }
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)  // 500
     @ExceptionHandler(Throwable.class)
-    public void handleAllExceptions() {
+    public void handleAllExceptions(final HttpServletRequest req, final Exception exception) {
         Thread.dumpStack();
         System.out.println("Global exception handler");
     }
