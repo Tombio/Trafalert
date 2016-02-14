3d9231a1e4be3a9e218305542f39f6276f348c1a
Tombio <tomijlahtinen@gmail.com>
2016-02-14 14:22:42 +0200

    Added parameter to hold API key

--- Server/deploy_localhost.sh
+++ Server/deploy_localhost.sh
@@ -1,2 +1,2 @@
 mvn clean install
-java -Dserver.url="http://localhost:8080" -jar target/Trafalert-1.0-SNAPSHOT.jar
+java -Dserver.url="http://localhost:8080" -DAPI_KEY="Your API key goes here" -jar target/Trafalert-1.0-SNAPSHOT.jar
